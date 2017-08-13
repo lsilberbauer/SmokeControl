17db28e914c6548295c8d17ae9a731076b6a57b8
lsilberbauer <lukas.silberbauer@taurob.com>
2017-08-13 18:07:13 +0200

    increased robustness, added last update

--- smokecontrol.py
+++ smokecontrol.py
@@ -8,34 +8,39 @@ import matplotlib.pyplot as plt
 import mpld3
 import threading
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import sys
+import datetime
 
 
 # HTTPRequestHandler class
 class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
     # GET
     def do_GET(self):
-        # Send response status code
-        self.send_response(200)
+        try:
+            # Send response status code
+            self.send_response(200)
 
-        # Send headers
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
+            # Send headers
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
 
-        # Generate plot
-        ax1.clear()
-        ax1.plot(probe1_temps)
-        ax1.plot(probe2_temps)
+            # Generate plot
+            ax1.clear()
+            ax1.plot(probe1_temps)
+            ax1.plot(probe2_temps)
 
-        # Send message back to client
-        message = "<html><meta http-equiv='refresh' content='3'><head><title>ThermoGraph</title></head><body>\r\n"
-        message += "<p>Probe 1: " + str(int(probe1_temps[len(probe1_temps) - 1])) + " &#8451</p>\r\n"
-        message += "<p>Probe 2: " + str(int(probe2_temps[len(probe2_temps) - 1])) + " &#8451</p>\r\n"
-        message += mpld3.fig_to_html(fig)
-        message += "</html>"
-        # Write content as utf-8 data
-        self.wfile.write(bytes(message, "utf8"))
+            # Send message back to client
+            message = "<html><meta http-equiv='refresh' content='3'><head><title>ThermoGraph</title></head><body>\r\n"
+            message += "<p>Probe 1: " + str(int(probe1_temps[len(probe1_temps) - 1])) + " &#8451</p>\r\n"
+            message += "<p>Probe 2: " + str(int(probe2_temps[len(probe2_temps) - 1])) + " &#8451</p>\r\n"
+            message += "<p>Last Update " + str(int((datetime.datetime.now() - last_received_temperatures).total_seconds())) +" seconds ago</p>"
+            message += mpld3.fig_to_html(fig)
+            message += "</html>"
+            # Write content as utf-8 data
+            self.wfile.write(bytes(message, "utf8"))
+        except:
+            print("Unexpected error:", sys.exc_info()[0])
         return
-
 
 def run():
     print('starting server...')
@@ -53,16 +58,24 @@ def fahrenheit_to_celsius(fahrenheit):
 
 def get_temperatures():
     while True:
-        xmldata = urllib.request.urlopen("http://192.168.178.31/THERMO/history?max=1").read()
-        parsedxml = xmltodict.parse(xmldata)
+        try:
+            xmldata = urllib.request.urlopen("http://192.168.178.31/THERMO/history?max=1").read()
+            parsedxml = xmltodict.parse(xmldata)
 
-        temp1 = fahrenheit_to_celsius(int(parsedxml['temps']['probes']['Temp'][0]['@temp']))
-        temp2 = fahrenheit_to_celsius(int(parsedxml['temps']['probes']['Temp'][1]['@temp']))
+            temp1 = fahrenheit_to_celsius(int(parsedxml['temps']['probes']['Temp'][0]['@temp']))
+            temp2 = fahrenheit_to_celsius(int(parsedxml['temps']['probes']['Temp'][1]['@temp']))
 
-        print ("Temp1: " + str(temp1) + " Temp2: " + str(temp2))
+            print("Temp1: " + str(temp1) + " Temp2: " + str(temp2))
 
-        probe1_temps.append(temp1)
-        probe2_temps.append(temp2)
+            probe1_temps.append(temp1)
+            probe2_temps.append(temp2)
+            global last_received_temperatures
+            last_received_temperatures = datetime.datetime.now()
+        except TimeoutError:
+            print("Timout Occured")
+        except:
+            print("Unexpected error:", sys.exc_info()[0])
+
         time.sleep(1)
     return
 
@@ -70,6 +83,7 @@ fig = plt.figure()
 ax1 = fig.add_subplot(1,1,1)
 probe1_temps = []
 probe2_temps = []
+last_received_temperatures = datetime.datetime.now()
 
 thread_get_temperatures = threading.Thread(target=get_temperatures)
 thread_get_temperatures.start()
